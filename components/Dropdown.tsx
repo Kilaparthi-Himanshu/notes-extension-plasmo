a6a9553fe8d36ba2b2e8258cccc024176a6adaf3
Himanshu <himanshunani9@gmail.com>
2025-01-05 14:33:43 +0530

    Added Dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Menu } from 'lucide-react';
 import styleText from "data-text:./styles.module.css";
 import * as style from "./styles.module.css";
+import { useState } from 'react';
 
 interface DropDownProps {
     theme: string;
@@ -14,14 +15,63 @@ export const getStyle = () => {
 }
 
 const Dropdown = ({ theme }: DropDownProps) => {
+    const [isOpen, setIsOpen] = useState(false);
     
     return (
-        <div className={style.hamburger}>
-            <Menu 
-                style={{
-                    color: theme === "light" ? "black" : "white"
-                }}
-            />
+        <div>
+            <div className={style.hamburger}>
+                <Menu 
+                    style={{
+                        color: isOpen 
+                                    ? "red"
+                                    : theme === "light" ? "black" : "white",
+                    }}
+                    onClick={() => setIsOpen(!isOpen)}
+                />
+            </div>
+            {isOpen && (
+                <div className={style.dropdown}>
+                    <div style={{
+                        backgroundColor: "gray",
+                        width: "90%",
+                        height: "50px",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        textAlign: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                        margin: "10px",
+                        cursor: "pointer",
+                        borderRadius: "10px",
+                    }}>GG</div>
+                    <div style={{
+                        backgroundColor: "gray",
+                        width: "90%",
+                        height: "50px",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        textAlign: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                        margin: "10px",
+                        cursor: "pointer",
+                        borderRadius: "10px",
+                    }}>GG</div>
+                    <div style={{
+                        backgroundColor: "gray",
+                        width: "90%",
+                        height: "50px",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        textAlign: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                        margin: "10px",
+                        cursor: "pointer",
+                        borderRadius: "10px",
+                    }}>GG</div>
+                </div>
+            )}
         </div>
     )
 }
